770bc78167ea38e97d3d9bae5c9c77d028144c85
yaman0808 <115275341+yaman0808@users.noreply.github.com>
2024-04-06 13:02:06 +0530

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 
 # Start the WebDriver and open the HTML page
